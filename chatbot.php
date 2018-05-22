f2d125b92f73d13c6334479cdc7043fe8e43aa22
Lanant <36738244+Lanant@users.noreply.github.com>
2018-05-22 08:52:48 +0200

    Update chatbot

--- chatbot.php
+++ chatbot.php
@@ -4,7 +4,7 @@
 $botToken = "601139420:AAF8bs4bZXiRLwE7N3OzXX8OqgkegvajbOw";
 $website = "https://api.telegram.org/bot".botToken;
 
-$update = file_get_contents($website."/getupdates");
+$update = file_get_contents("php://input");
 
 $updateArray = json_decode($update, TRUE);
 
